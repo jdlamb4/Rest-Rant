08c850ae44bb911c7c0ac9f23ff87e6e2d9ac10a
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-04 20:35:22 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-// Express Settings
+// Express Settings 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
--- views/home.jsx
+++ views/home.jsx
@@ -1,6 +1,6 @@
 const React = require("react");
 const Def = require("./default");
-
+ 
 function home() {
   return (
     <Def>
