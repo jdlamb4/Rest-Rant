80eb4ef8845935c150cc56c52c47a0a63df91acc
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-04 23:19:39 -0400

    form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -20,6 +20,12 @@ router.get('/', function(req, res) {
     res.render('places/index', { places })
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
