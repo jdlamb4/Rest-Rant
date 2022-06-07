67a2c07132de6b0d538b0c0790a6fa8cecb0a402
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-06 21:34:22 -0400

    Part 7 (BE 5) added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -36,10 +36,25 @@ router.post("/", (req, res) => {
   res.redirect("/places");
 });
 
+
 // GET / places/new Route
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
+
+// Edit Route
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
