a1bc30e85a42a6de1f298db62537293dee7b94a6
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-11 19:24:34 -0400

    Started Part 8 - Implementing the Mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -1,114 +1,187 @@
-//
-const router = require("express").Router();
-const places = require("../models/places.js");
+const router = require('express').Router()
+const db = require('../models')
 
 // Index Route
-router.get("/", function (req, res) {
-  // let places = [{
-  //     name: 'H-Thai-ML',
-  //     city: 'Seattle',
-  //     state: 'WA',
-  //     cuisines: 'Thai, Pan-Asian',
-  //     pic: '/images/HThaiML.jpeg'
-  //   }, {
-  //     name: 'Coding Cat Cafe',
-  //     city: 'Phoenix',
-  //     state: 'AZ',
-  //     cuisines: 'Coffee, Bakery',
-  //     pic: '/images/cafe.jpeg'
-  //   }]
-  res.render("places/index", { places });
-});
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
 
-//  Post /places route
-router.post("/", (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = "http://placekitten.com/400/400";
-  } 
-  if (!req.body.city) {
-    req.body.city = "Anytown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body)
-  res.redirect("/places");
-});
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
 
-
-// GET / places/new Route
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
-
-// Edit Route
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id })
-  }
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
+  res.send('GET /places/:id stub')
 })
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
-
 
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  res.send('DELETE /places/:id stub')
 })
 
-module.exports = router;
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// const router = require("express").Router();
+// const places = require("../models/places.js");
+
+// // Index Route
+// router.get("/", function (req, res) {
+//   // let places = [{
+//   //     name: 'H-Thai-ML',
+//   //     city: 'Seattle',
+//   //     state: 'WA',
+//   //     cuisines: 'Thai, Pan-Asian',
+//   //     pic: '/images/HThaiML.jpeg'
+//   //   }, {
+//   //     name: 'Coding Cat Cafe',
+//   //     city: 'Phoenix',
+//   //     state: 'AZ',
+//   //     cuisines: 'Coffee, Bakery',
+//   //     pic: '/images/cafe.jpeg'
+//   //   }]
+//   res.render("places/index", { places });
+// });
+
+// //  Post /places route
+// router.post("/", (req, res) => {
+//   if (!req.body.pic) {
+//     // Default image if one is not provided
+//     req.body.pic = "http://placekitten.com/400/400";
+//   } 
+//   if (!req.body.city) {
+//     req.body.city = "Anytown";
+//   }
+//   if (!req.body.state) {
+//     req.body.state = "USA";
+//   }
+//   places.push(req.body)
+//   res.redirect("/places");
+// });
+
+
+// // GET / places/new Route
+// router.get("/new", (req, res) => {
+//   res.render("places/new");
+// });
+
+// // Edit Route
+// router.get('/:id/edit', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/edit', { place: places[id], id })
+//   }
+// })
+
+// router.get('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
+
+// router.put('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//       res.render('error404')
+//   }
+//   else if (!places[id]) {
+//       res.render('error404')
+//   }
+//   else {
+//       // Dig into req.body and make sure data is valid
+//       if (!req.body.pic) {
+//           // Default image if one is not provided
+//           req.body.pic = 'http://placekitten.com/400/400'
+//       }
+//       if (!req.body.city) {
+//           req.body.city = 'Anytown'
+//       }
+//       if (!req.body.state) {
+//           req.body.state = 'USA'
+//       }
+
+//       // Save the new data into places[id]
+//       places[id] = req.body
+//       res.redirect(`/places/${id}`)
+//   }
+// })
+
+
+// router.delete('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     places.splice(id, 1)
+//     res.redirect('/places')
+//   }
+// })
+
+// module.exports = router;
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,6 +1,6 @@
 const React = require("react");
 const Def = require("../default");
- 
+
 function new_form() {
   return (
     <Def>
@@ -9,7 +9,7 @@ function new_form() {
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required/>
+            <input className="form-control" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
@@ -24,8 +24,17 @@ function new_form() {
             <input className="form-control" id="state" name="state" />
           </div>
           <div className="form-group">
+            <label for="founded">Founded Year</label>
+            <input className="form-control" id="founded" name="founded" />
+          </div>
+          <div className="form-group">
             <label htmlFor="Cuisines">Place Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required/>
+            <input
+              className="form-control"
+              id="cuisines"
+              name="cuisines"
+              required
+            />
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
