05aefdf0cb6a45c752db94beae49bfea0d697792
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-13 00:11:25 -0400

    Finished Part 10 - need to style sliding scale and checkbox

--- controllers/places.js
+++ controllers/places.js
@@ -30,7 +30,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -49,6 +51,27 @@ router.delete('/:id', (req, res) => {
 
 router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 router.post('/:id/rant', (req, res) => {
